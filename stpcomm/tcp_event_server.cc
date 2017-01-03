272fcfbc4e1921a80fe8003581882af916223c90
wx7217242 <wx7217242@sina.com>
2017-01-03 22:03:42 +0800

    把Connection和LibeventThread从tcp_event_server.cc中剥离出来

--- stpcomm/tcp_event_server.cc
+++ stpcomm/tcp_event_server.cc
@@ -1,87 +1,22 @@
 #include "tcp_event_server.h"
 
-Connection::Connection(int fd) : fd_(fd)
-{
-	prev_conn_ = NULL;
-	next_conn_ = NULL;
-}
-
-Connection::~Connection()
-{
-
-}
-
-ConnectionQueue::ConnectionQueue()
-{
-	//建立头尾结点，并调整其指针
-	head_ = new Connection(0);
-	tail_ = new Connection(0);
-	head_->prev_conn_ = tail_->next_conn_ = NULL;
-	head_->next_conn_ = tail_;
-	tail_->prev_conn_ = head_;
-}
-
-ConnectionQueue::~ConnectionQueue()
-{
-	Connection *tcur, *tnext;
-	tcur = head_;
-	//循环删除链表中的各个结点
-	while( tcur != NULL )
-	{
-		tnext = tcur->next_conn_;
-		delete tcur;
-		tcur = tnext;
-	}
-}
-
-Connection *ConnectionQueue::InsertConnection(int fd, LibeventThread *t)
-{
-	Connection *c = new Connection(fd);
-	c->thread_ = t;
-	Connection *next = head_->next_conn_;
-
-	c->prev_conn_ = head_;
-	c->next_conn_ = head_->next_conn_;
-	head_->next_conn_ = c;
-	next->prev_conn_ = c;
-	return c;
-}
-
-void ConnectionQueue::DeleteConnection(Connection *c)
-{
-	c->prev_conn_->next_conn_ = c->next_conn_;
-	c->next_conn_->prev_conn_ = c->prev_conn_;
-	delete c;
-}
-
-/*
-void ConnQueue::PrintQueue()
-{
-	Conn *cur = m_head->m_Next;
-	while( cur->m_Next != NULL )
-	{
-		printf("%d ", cur->m_fd);
-		cur = cur->m_Next;
-	}
-	printf("\n");
-}
-*/
+#include <glog/logging.h>
+#include <assert.h>
 
 TcpEventServer::TcpEventServer(int16_t port, int thread_cnt)
     : port_(port), thread_cnt_(thread_cnt)
 {
 	//初始化各项数据
 //	m_MainBase = new LibeventThread;
-	threads_ = new LibeventThread[thread_cnt_];
+//	threads_ = new LibeventThread[thread_cnt_];
 	main_base_.thread_id = pthread_self();
 	main_base_.base = event_base_new();
 
 	//初始化各个子线程的结构体
-	for(int i=0; i<thread_cnt_; i++)
+	for(int i=0; i < thread_cnt_; i++)
 	{
-		SetupThread(&threads_[i]);
+		threads_.push_back(new LibeventThread(this));
 	}
-
 }
 
 TcpEventServer::~TcpEventServer()
@@ -91,10 +26,11 @@ TcpEventServer::~TcpEventServer()
 
 	//释放内存
 	event_base_free(main_base_.base);
-	for(int i=0; i<thread_cnt_; i++)
-		event_base_free(threads_[i].thread_data_.base);
+	for(int i=0; i < thread_cnt_; ++i)
+        delete threads_[i];
+		// threads_[i].FreeEventBase();
 
-	delete [] threads_;
+//	delete [] threads_;
 }
 
 void TcpEventServer::ErrorQuit(const char *str)
@@ -105,32 +41,6 @@ void TcpEventServer::ErrorQuit(const char *str)
 		fprintf(stderr, " : %s", strerror(errno));    
 	fprintf(stderr, "\n");        
 	exit(1);    
-}
-
-void TcpEventServer::SetupThread(LibeventThread *me)
-{
-	//建立libevent事件处理机制    
-    LibeventThreadData& thread_data = me->thread_data_;
-    
-    thread_data.tcp_event_server = this;
-	thread_data.base = event_base_new();
-	if( NULL == thread_data.base )
-		ErrorQuit("event base new error");
-
-	//在主线程和子线程之间建立管道
-	int fds[2];
-	if( pipe(fds) )
-		ErrorQuit("create pipe error");
-	thread_data.notify_recv_fd = fds[0];
-	thread_data.notify_send_fd = fds[1];
-
-	//让子线程的状态机监听管道
-	event_set( &thread_data.notify_event, thread_data.notify_recv_fd,
-		EV_READ | EV_PERSIST, ThreadProcess, me );
-    
-	event_base_set(thread_data.base, &thread_data.notify_event);
-	if ( event_add(&thread_data.notify_event, 0) == -1 )
-		ErrorQuit("Can't monitor libevent notify pipe\n");
 }
 
 bool TcpEventServer::StartRun()
@@ -145,7 +55,7 @@ bool TcpEventServer::StartRun()
 		sin.sin_family = AF_INET;
 		sin.sin_port = htons(port_);
 		listener = evconnlistener_new_bind(main_base_.base, 
-			ListenerEventCb, this,
+			ListenerEventCallback, this,
 			LEV_OPT_REUSEABLE|LEV_OPT_CLOSE_ON_FREE, -1,
 			reinterpret_cast<sockaddr*>(&sin), sizeof(sockaddr_in));
 		if( NULL == listener )
@@ -155,9 +65,7 @@ bool TcpEventServer::StartRun()
 	//开启各个子线程
 	for(int i=0; i<thread_cnt_; i++)
 	{
-//		pthread_create(&m_Threads[i].tid, NULL,  
-//			WorkerLibevent, reinterpret_cast<void*>(&m_Threads[i]));
-        threads_[i].Start();
+        threads_[i]->Start();
 	}
 
 	//开启主线程的事件循环
@@ -174,17 +82,16 @@ bool TcpEventServer::StartRun()
 
 void TcpEventServer::StopRun(timeval *tv)
 {
-	int contant = EXIT_CODE;
 	//向各个子线程的管理中写入EXIT_CODE，通知它们退出
 	for(int i=0; i<thread_cnt_; i++)
 	{
-		write(threads_[i].thread_data_.notify_send_fd, &contant, sizeof(int));
+        threads_[i]->SignalExit();
 	}
 	//结果主线程的事件循环
 	event_base_loopexit(main_base_.base, tv);
 }
 
-void TcpEventServer::ListenerEventCb(struct evconnlistener *listener, 
+void TcpEventServer::ListenerEventCallback(struct evconnlistener *listener, 
 									evutil_socket_t fd,
 									struct sockaddr *sa, 
 									int socklen, 
@@ -194,48 +101,10 @@ void TcpEventServer::ListenerEventCb(struct evconnlistener *listener,
 
 	//随机选择一个子线程，通过管道向其传递socket描述符
 	int num = rand() % server->thread_cnt_;
-	int sendfd = server->threads_[num].thread_data_.notify_send_fd;
-	write(sendfd, &fd, sizeof(evutil_socket_t));
+    server->threads_[num]->ProcessListenerEvent(fd);
+
 }
 
-void TcpEventServer::ThreadProcess(int fd, short which, void *arg)
-{
-	LibeventThread *me = reinterpret_cast<LibeventThread*>(arg);
-    LibeventThreadData& thread_data = me->thread_data_;
-
-	//从管道中读取数据（socket的描述符或操作码）
-	int pipefd = thread_data.notify_recv_fd;
-	evutil_socket_t confd;
-	read(pipefd, &confd, sizeof(evutil_socket_t));
-
-	//如果操作码是EXIT_CODE，则终于事件循环
-	if( EXIT_CODE == confd )
-	{
-		event_base_loopbreak(thread_data.base);
-		return;
-	}
-
-	//新建连接
-	struct bufferevent *bev;
-	bev = bufferevent_socket_new(thread_data.base, confd, BEV_OPT_CLOSE_ON_FREE);
-	if (!bev)
-	{
-		fprintf(stderr, "Error constructing bufferevent!");
-		event_base_loopbreak(thread_data.base);
-		return;
-	}
-
-	//将该链接放入队列
-	Connection *conn = thread_data.conn_queue.InsertConnection(confd, me);
-
-	//准备从socket中读写数据
-	bufferevent_setcb(bev, ReadEventCallback, WriteEventCallback, CloseEventCallback, conn);
-	bufferevent_enable(bev, EV_WRITE);
-	bufferevent_enable(bev, EV_READ);
-
-	//调用用户自定义的连接事件处理函数
-	me->thread_data_.tcp_event_server->ConnectionEvent(conn);
-}
 
 void TcpEventServer::ReadEventCallback(struct bufferevent *bev, void *data)
 {
@@ -245,7 +114,7 @@ void TcpEventServer::ReadEventCallback(struct bufferevent *bev, void *data)
 	conn->write_buf_ = bufferevent_get_output(bev);
 
 	//调用用户自定义的读取事件处理函数
-	conn->thread_->thread_data_.tcp_event_server->ReadEvent(conn);
+    conn->ProcessReadEvent();
 } 
 
 void TcpEventServer::WriteEventCallback(struct bufferevent *bev, void *data)
@@ -255,16 +124,17 @@ void TcpEventServer::WriteEventCallback(struct bufferevent *bev, void *data)
 	conn->write_buf_ = bufferevent_get_output(bev);
 
 	//调用用户自定义的写入事件处理函数
-	conn->thread_->thread_data_.tcp_event_server->WriteEvent(conn);
-
+//	conn->thread_->thread_data_.tcp_event_server->WriteEvent(conn);
+    conn->ProcessWriteEvent();
 }
 
 void TcpEventServer::CloseEventCallback(struct bufferevent *bev, short events, void *data)
 {
 	Connection *conn = reinterpret_cast<Connection*>(data);
 	//调用用户自定义的断开事件处理函数
-	conn->thread_->thread_data_.tcp_event_server->CloseEvent(conn, events);
-	conn->thread()->thread_data_.conn_queue.DeleteConnection(conn);
+//	conn->thread_->thread_data_.tcp_event_server->CloseEvent(conn, events);
+    conn->ProcessCloseEvent(events);
+	
 	bufferevent_free(bev);
 }
 
@@ -317,12 +187,4 @@ event *TcpEventServer::AddTimerEvent(void (*ptr)(int, short, void *),
 bool TcpEventServer::DeleteTimerEvent(event *ev)
 {
     return  event_del(ev) == 0;
-}
-void LibeventThread::Run()
-{
-    //开启libevent的事件循环，准备处理业务
-//	LibeventThread *me = reinterpret_cast<LibeventThread*>(arg);
-	printf("thread %lu started\n", thread_data_.thread_id);
-	event_base_dispatch(thread_data_.base);
-	printf("subthread done\n");
 }
--- stpcomm/tcp_event_server.h
+++ stpcomm/tcp_event_server.h
@@ -6,109 +6,16 @@
 #include <errno.h>
 #include <signal.h>
 
+#include <vector>
 #include <map>
 
 #include <event.h>
 #include <event2/listener.h>
 
 #include "stpcomm/thread.h"
+#include "libevent_thread.h"
 
-class TcpEventServer;
 class Connection;
-class ConnectionQueue;
-struct LibeventThread;
-
-//这个类一个链表的结点类，结点里存储各个连接的信息，
-//并提供了读写数据的接口
-class Connection
-{
-	//此类只能由TcpBaseServer创建，
-	//并由ConnQueue类管理
-	friend class ConnectionQueue;
-	friend class TcpEventServer;
-
-private:
-	const int fd_;				//socket的ID
-	evbuffer *read_buf_;		//读数据的缓冲区
-	evbuffer *write_buf_;		//写数据的缓冲区
-
-	Connection *prev_conn_;				//前一个结点的指针
-	Connection *next_conn_;				//后一个结点的指针
-	LibeventThread *thread_;
-
-	Connection(int fd = 0);
-	~Connection();
-
-public:
-	LibeventThread *thread() { return thread_; }
-	int GetFd() { return fd_; }
-
-	//获取可读数据的长度
-	size_t GetReadBufferLen()
-	{ return evbuffer_get_length(read_buf_); }
-
-	//从读缓冲区中取出len个字节的数据，存入buffer中，若不够，则读出所有数据
-	//返回读出数据的字节数
-	int GetReadBuffer(char *buffer, int len)
-	{ return evbuffer_remove(read_buf_, buffer, len); }
-
-	//从读缓冲区中复制出len个字节的数据，存入buffer中，若不够，则复制出所有数据
-	//返回复制出数据的字节数
-	//执行该操作后，数据还会留在缓冲区中，buffer中的数据只是原数据的副本
-	size_t CopyReadBuffer(char *buffer, int len)
-	{ return evbuffer_copyout(read_buf_, buffer, len); }
-
-	//获取可写数据的长度
-	size_t GetWriteBufferLen()
-	{ return evbuffer_get_length(write_buf_); }
-
-	//将数据加入写缓冲区，准备发送
-	int AddToWriteBuffer(char *buffer, size_t len)
-	{ return evbuffer_add(write_buf_, buffer, len); }
-
-	//将读缓冲区中的数据移动到写缓冲区
-	void MoveBufferData()
-	{ evbuffer_add_buffer(write_buf_, read_buf_); }
-
-};
-
-//带头尾结点的双链表类，每个结点存储一个连接的数据
-class ConnectionQueue
-{
-public:
-	ConnectionQueue();
-	~ConnectionQueue();
-    
-	Connection* InsertConnection(int fd, LibeventThread* t);
-	void DeleteConnection(Connection *c);
-    
-	//void PrintQueue();
-private:
-	Connection *head_;
-	Connection *tail_;
-};
-
-struct LibeventThreadData
-{
-	unsigned long thread_id;				//线程的ID
-	struct event_base* base;	//libevent的事件处理机
-	struct event notify_event;	//监听管理的事件机
-	int notify_recv_fd;		//管理的接收端
-	int notify_send_fd;			//管道的发送端
-	ConnectionQueue conn_queue;		//socket连接的链表
-	//在libevent的事件处理中要用到很多回调函数，不能使用类隐含的this指针
-	//所以用这样方式将TcpBaseServer的类指针传过去
-	TcpEventServer *tcp_event_server;	 //TcpBaseServer类的指针
-};
-
-class LibeventThread : public Thread
-{
-public:
-    virtual void Run();
-public:
-    LibeventThreadData thread_data_;
-};
-
 class TcpEventServer
 {
 public:
@@ -138,6 +45,13 @@ public:
 	event *AddTimerEvent(void(*ptr)(int, short, void*), timeval tv, bool once);
 	bool DeleteTimerEvent(event *ev);
     
+    //被libevent回调的各个静态函数
+	static void ListenerEventCallback(evconnlistener *listener, evutil_socket_t fd, 
+                                      sockaddr *sa, int socklen, void *user_data);
+	static void ReadEventCallback(struct bufferevent *bev, void *data);
+	static void WriteEventCallback(struct bufferevent *bev, void *data); 
+	static void CloseEventCallback(struct bufferevent *bev, short events, void *data);
+    
 private:
     int16_t port_;							//监听的端口
 	int thread_cnt_;					//子线程数
@@ -145,24 +59,10 @@ private:
 //	LibeventThread *m_Threads;			//存储各个子线程信息的数组
     
     LibeventThreadData main_base_;
-    LibeventThread* threads_;
+    std::vector<LibeventThread*> threads_;
     
 	std::map<int, event*> signal_events_;	//自定义的信号处理
-    
-private:
-	//初始化子线程的数据
-	void SetupThread(LibeventThread *thread);
-
-	//（主线程收到请求后），对应子线程的处理函数
-	static void ThreadProcess(int fd, short which, void *arg);
-	//被libevent回调的各个静态函数
-	static void ListenerEventCb(evconnlistener *listener, evutil_socket_t fd,
-		sockaddr *sa, int socklen, void *user_data);
-	static void ReadEventCallback(struct bufferevent *bev, void *data);
-	static void WriteEventCallback(struct bufferevent *bev, void *data); 
-	static void CloseEventCallback(struct bufferevent *bev, short events, void *data);
-
-protected:
+public:
 	//这五个虚函数，一般是要被子类继承，并在其中处理具体业务的
 
 	//新建连接成功后，会调用该函数
