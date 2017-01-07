80b16ce91a9d73c7b8b70870e0a1fcd55d096d7f
wx7217242 <wx7217242@sina.com>
2017-01-07 13:15:16 +0800

    Connection中增加删除节点

--- stpcomm/connection.cc
+++ stpcomm/connection.cc
@@ -1,13 +1,94 @@
 #include "connection.h"
 #include "libevent_thread.h"
-#include "tcp_event_server.h"
+#include "event_notifier.h"
+
+#include <assert.h>
+
+Connection::Connection(EventNotifier *notifier, int fd) 
+    : event_notifier_(notifier), fd_(fd), prev_conn_(NULL), next_conn_(NULL)
+{
+    
+}
+
+Connection::~Connection() 
+{
+    
+}
+
+void Connection::ProcessReadEvent() 
+{ 
+    event_notifier_->HandleReadEvent(this); 
+}
+
+void Connection::ProcessWriteEvent()
+{ 
+    event_notifier_->HandleWriteEvent(this); 
+}
+
+void Connection::ProcessCloseEvent(short events)
+{ 
+    event_notifier_->HandleCloseEvent(this, events); 
+}
+
+void Connection::DeleteConnection(Connection *c)
+{
+    assert(c != NULL);
+    // TODO 优化
+    // 判断的原因是因为TcpEventClient的prev_conn_和next_conn_都为NULL
+    if (c->prev_conn_ != NULL)
+    {
+        c->prev_conn_->next_conn_ = c->next_conn_;
+    }
+    if (c->next_conn_ != NULL)
+    {
+        c->next_conn_->prev_conn_ = c->prev_conn_;
+    }
+    delete c;
+}
+
+void Connection::SetBuffer(evbuffer *read_buf, evbuffer *write_buf)
+{
+    assert(read_buf != NULL && write_buf != NULL);
+    read_buf_ = read_buf;
+    write_buf_ = write_buf;
+}
+
+size_t Connection::GetReadBufferLen()
+{ 
+    return evbuffer_get_length(read_buf_); 
+}
+
+int Connection::GetReadBuffer(char *buffer, int len)
+{ 
+    return evbuffer_remove(read_buf_, buffer, len); 
+}
+
+size_t Connection::CopyReadBuffer(char *buffer, int len)
+{ 
+    return evbuffer_copyout(read_buf_, buffer, len); 
+}
+
+size_t Connection::GetWriteBufferLen()
+{ 
+    return evbuffer_get_length(write_buf_); 
+}
+
+int Connection::AddToWriteBuffer(char *buffer, size_t len)
+{ 
+    return evbuffer_add(write_buf_, buffer, len); 
+}
+
+void Connection::MoveBufferData()
+{ 
+    evbuffer_add_buffer(write_buf_, read_buf_); 
+}
 
 
 ConnectionQueue::ConnectionQueue()
 {
-	//建立头尾结点，并调整其指针
-	head_ = new Connection(0);
-	tail_ = new Connection(0);
+	// 建立头尾结点，并调整其指针
+	head_ = new Connection(NULL);
+	tail_ = new Connection(NULL);
 	head_->prev_conn_ = tail_->next_conn_ = NULL;
 	head_->next_conn_ = tail_;
 	tail_->prev_conn_ = head_;
@@ -15,20 +96,23 @@ ConnectionQueue::ConnectionQueue()
 
 ConnectionQueue::~ConnectionQueue()
 {
-	Connection *tcur, *tnext;
-	tcur = head_;
-	//循环删除链表中的各个结点
-	while( tcur != NULL )
+	Connection *cur, *next;
+	cur = head_;
+	// 循环删除链表中的各个结点
+	while(cur != NULL)
 	{
-		tnext = tcur->next_conn_;
-		delete tcur;
-		tcur = tnext;
+		next = cur->next_conn_;
+		delete cur;
+		cur = next;
 	}
 }
 
-Connection *ConnectionQueue::InsertConnection(int fd, LibeventThread *t)
+Connection *ConnectionQueue::NewConnection(EventNotifier *notifier, int fd)
 {
-	Connection *c = new Connection(t->GetTcpEventServer(), fd);
+	Connection *c = new Connection(notifier, fd);
+    if (c == NULL)
+        return NULL;
+    
 	Connection *next = head_->next_conn_;
 
 	c->prev_conn_ = head_;
@@ -40,20 +124,8 @@ Connection *ConnectionQueue::InsertConnection(int fd, LibeventThread *t)
 
 void ConnectionQueue::DeleteConnection(Connection *c)
 {
+    assert(c != NULL);
 	c->prev_conn_->next_conn_ = c->next_conn_;
 	c->next_conn_->prev_conn_ = c->prev_conn_;
 	delete c;
 }
-
-/*
-void ConnQueue::PrintQueue()
-{
-	Conn *cur = m_head->m_Next;
-	while( cur->m_Next != NULL )
-	{
-		printf("%d ", cur->m_fd);
-		cur = cur->m_Next;
-	}
-	printf("\n");
-}
-*/
--- stpcomm/connection.h
+++ stpcomm/connection.h
@@ -11,68 +11,53 @@ class Connection
 {
     friend class ConnectionQueue;
 public:
-    Connection(EventNotifier* notifier, int fd = 0) 
-        : event_notifier_(notifier), fd_(fd), prev_conn_(NULL), next_conn_(NULL)
-    {
-        
-    }
-	~Connection() {}
+    Connection(EventNotifier* notifier, int fd = -1);
     
-    void ProcessReadEvent() 
-    { event_notifier_->HandleReadEvent(this); }
+	~Connection();
     
-    void ProcessWriteEvent()
-    { event_notifier_->HandleWriteEvent(this); }
+    void ProcessReadEvent();
     
-    void ProcessCloseEvent(short events)
-    { event_notifier_->HandleCloseEvent(this, events); }
+    void ProcessWriteEvent();
+    
+    void ProcessCloseEvent(short events);
+    
+    static void DeleteConnection(Connection *c);
 
+    void SetFd(int fd) { fd_ = fd; }
 	int GetFd() { return fd_; }
     
     EventNotifier* event_notifier() { return event_notifier_; }
     
-    void SetBuffer(evbuffer *read_buf, evbuffer *write_buf)
-    {
-        read_buf_ = read_buf;
-        write_buf_ = write_buf;
-    }
+    void SetBuffer(evbuffer *read_buf, evbuffer *write_buf);
 
-	//获取可读数据的长度
-	size_t GetReadBufferLen()
-	{ return evbuffer_get_length(read_buf_); }
+	// 获取可读数据的长度
+	size_t GetReadBufferLen();
 
-	//从读缓冲区中取出len个字节的数据，存入buffer中，若不够，则读出所有数据
-	//返回读出数据的字节数
-	int GetReadBuffer(char *buffer, int len)
-	{ return evbuffer_remove(read_buf_, buffer, len); }
+	// 从读缓冲区中取出len个字节的数据，存入buffer中，若不够，则读出所有数据
+	// 返回读出数据的字节数
+	int GetReadBuffer(char *buffer, int len);
 
-	//从读缓冲区中复制出len个字节的数据，存入buffer中，若不够，则复制出所有数据
-	//返回复制出数据的字节数
-	//执行该操作后，数据还会留在缓冲区中，buffer中的数据只是原数据的副本
-	size_t CopyReadBuffer(char *buffer, int len)
-	{ return evbuffer_copyout(read_buf_, buffer, len); }
+	// 从读缓冲区中复制出len个字节的数据，存入buffer中，若不够，则复制出所有数据
+	// 返回复制出数据的字节数
+	// 执行该操作后，数据还会留在缓冲区中，buffer中的数据只是原数据的副本
+	size_t CopyReadBuffer(char *buffer, int len);
 
-	//获取可写数据的长度
-	size_t GetWriteBufferLen()
-	{ return evbuffer_get_length(write_buf_); }
+	// 获取可写数据的长度
+	size_t GetWriteBufferLen();
 
-	//将数据加入写缓冲区，准备发送
-	int AddToWriteBuffer(char *buffer, size_t len)
-	{ return evbuffer_add(write_buf_, buffer, len); }
+	// 将数据加入写缓冲区，准备发送
+	int AddToWriteBuffer(char *buffer, size_t len);
 
-	//将读缓冲区中的数据移动到写缓冲区
-	void MoveBufferData()
-	{ evbuffer_add_buffer(write_buf_, read_buf_); }
+	// 将读缓冲区中的数据移动到写缓冲区
+	void MoveBufferData();
     
-protected:
+private:
     EventNotifier* event_notifier_;
-    const int fd_;				//socket的ID
+    int fd_;				//socket的ID
 	evbuffer *read_buf_;		//读数据的缓冲区
 	evbuffer *write_buf_;		//写数据的缓冲区
     Connection *prev_conn_;				//前一个结点的指针
 	Connection *next_conn_;				//后一个结点的指针
-private:
-    
 };
 
 
@@ -83,7 +68,7 @@ public:
 	ConnectionQueue();
 	~ConnectionQueue();
     
-	Connection* InsertConnection(int fd, LibeventThread* t);
+	Connection* NewConnection(EventNotifier* notifier, int fd);
 	void DeleteConnection(Connection *c);
     
 	//void PrintQueue();
