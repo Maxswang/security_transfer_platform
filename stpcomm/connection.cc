6fd1743d25fbde12c67eb96bed0f0ad312efaf72
wx7217242 <wx7217242@sina.com>
2017-01-08 17:00:17 +0800

    Connection网络消息打包和解包

--- stpcomm/connection.cc
+++ stpcomm/connection.cc
@@ -2,6 +2,7 @@
 #include "libevent_thread.h"
 #include "event_notifier.h"
 
+#include <string.h>
 #include <assert.h>
 
 Connection::Connection(EventNotifier *notifier, int fd) 
@@ -16,8 +17,11 @@ Connection::~Connection()
 }
 
 void Connection::ProcessReadEvent() 
-{ 
-    event_notifier_->HandleReadEvent(this); 
+{
+    if (UnpackNetHeadPacket())
+    {
+        event_notifier_->HandleReadEvent(this);
+    }
 }
 
 void Connection::ProcessWriteEvent()
@@ -49,38 +53,103 @@ void Connection::DeleteConnection(Connection *c)
 void Connection::SetBuffer(evbuffer *read_buf, evbuffer *write_buf)
 {
     assert(read_buf != NULL && write_buf != NULL);
-    read_buf_ = read_buf;
-    write_buf_ = write_buf;
+    ev_read_buf_ = read_buf;
+    ev_write_buf_ = write_buf;
 }
 
 size_t Connection::GetReadBufferLen()
 { 
-    return evbuffer_get_length(read_buf_); 
+    return evbuffer_get_length(ev_read_buf_); 
 }
 
-int Connection::GetReadBuffer(char *buffer, int len)
+int Connection::GetReadBuffer(void *buffer, size_t len)
 { 
-    return evbuffer_remove(read_buf_, buffer, len); 
+    return evbuffer_remove(ev_read_buf_, buffer, len); 
 }
 
-size_t Connection::CopyReadBuffer(char *buffer, int len)
+size_t Connection::CopyReadBuffer(void *buffer, size_t len)
 { 
-    return evbuffer_copyout(read_buf_, buffer, len); 
+    return evbuffer_copyout(ev_read_buf_, buffer, len); 
 }
 
 size_t Connection::GetWriteBufferLen()
 { 
-    return evbuffer_get_length(write_buf_); 
+    return evbuffer_get_length(ev_write_buf_); 
 }
 
-int Connection::AddToWriteBuffer(char *buffer, size_t len)
+int Connection::AddToWriteBuffer(const void *buffer, size_t len)
 { 
-    return evbuffer_add(write_buf_, buffer, len); 
+    return evbuffer_add(ev_write_buf_, buffer, len); 
 }
 
 void Connection::MoveBufferData()
 { 
-    evbuffer_add_buffer(write_buf_, read_buf_); 
+    evbuffer_add_buffer(ev_write_buf_, ev_read_buf_); 
+}
+
+bool Connection::UnpackNetHeadPacket()
+{
+    size_t head_size = sizeof(NetHeadPacket);
+    size_t read_buf_len = GetReadBufferLen();
+    if (read_buf_len < head_size)
+        return false;
+    
+    NetHeadPacket head;
+    memset(&head, 0, sizeof(head));
+    CopyReadBuffer(&head, head_size);
+    
+    head.version = ntohs(head.version);
+    head.pkt_len = ntohl(head.pkt_len);
+    size_t pkt_len = head.pkt_len;
+    
+    if (read_buf_len < pkt_len + head_size)
+    {
+        return false;
+    }
+    
+    GetReadBuffer(&head, head_size);
+    
+    read_buf_list_.push_back(std::string(""));
+    std::string& pkt_body = read_buf_list_.back();
+    pkt_body.resize(pkt_len);
+    
+    while (pkt_len > 0) 
+    {
+        int read = static_cast<int>(std::min(sizeof(read_buf_), pkt_len));
+        if (GetReadBuffer(read_buf_, read) == 0)
+        {
+            break;
+        }
+        pkt_len -= read;
+        pkt_body.append(read_buf_, read_buf_ + read);
+    }
+    return true;
+}
+
+bool Connection::PackNetHeadPacket(const void *buffer, size_t len)
+{
+    NetHeadPacket head;
+    memset(&head, 0, sizeof(head));
+    head.version = htons(kNetHeadPacketVersion);
+    head.pkt_len = htonl(len);
+    
+    AddToWriteBuffer(&head, sizeof(head));
+    AddToWriteBuffer(buffer, len);
+    
+    return true;
+}
+
+bool Connection::GetOneUnpackedPacket(std::string& packet)
+{
+    if (read_buf_list_.empty())
+    {
+        return false;
+    }
+    
+    packet = read_buf_list_.front();
+    read_buf_list_.pop_front();
+    
+    return true;
 }
 
 
--- stpcomm/connection.h
+++ stpcomm/connection.h
@@ -5,12 +5,28 @@
 
 #include "event_notifier.h"
 
+#include <list>
+#include <string>
+
 class LibeventThread;
+
+const uint8_t kNetHeadPacketVersion = 10;
+
+#pragma pack(2) 
+struct NetHeadPacket
+{
+    uint8_t version;
+    uint32_t pkt_len;
+};
+#pragma pack()
 
 class Connection
 {
     friend class ConnectionQueue;
 public:
+    typedef std::list<std::string> BufferList;
+    typedef BufferList::iterator BufferListIterator;
+    
     Connection(EventNotifier* notifier, int fd = -1);
     
 	~Connection();
@@ -35,29 +51,40 @@ public:
 
 	// 从读缓冲区中取出len个字节的数据，存入buffer中，若不够，则读出所有数据
 	// 返回读出数据的字节数
-	int GetReadBuffer(char *buffer, int len);
+	int GetReadBuffer(void *buffer, size_t len);
 
 	// 从读缓冲区中复制出len个字节的数据，存入buffer中，若不够，则复制出所有数据
 	// 返回复制出数据的字节数
 	// 执行该操作后，数据还会留在缓冲区中，buffer中的数据只是原数据的副本
-	size_t CopyReadBuffer(char *buffer, int len);
+	size_t CopyReadBuffer(void *buffer, size_t len);
 
 	// 获取可写数据的长度
 	size_t GetWriteBufferLen();
 
 	// 将数据加入写缓冲区，准备发送
-	int AddToWriteBuffer(char *buffer, size_t len);
+	int AddToWriteBuffer(const void *buffer, size_t len);
 
 	// 将读缓冲区中的数据移动到写缓冲区
 	void MoveBufferData();
     
+    bool UnpackNetHeadPacket();
+    bool PackNetHeadPacket(const void* buffer, size_t len);
+    bool GetOneUnpackedPacket(std::string &packet);
+    void DelteUnpacketPacket(BufferListIterator& it);
+    
 private:
     EventNotifier* event_notifier_;
     int fd_;				//socket的ID
-	evbuffer *read_buf_;		//读数据的缓冲区
-	evbuffer *write_buf_;		//写数据的缓冲区
+	evbuffer *ev_read_buf_;		//读数据的缓冲区
+	evbuffer *ev_write_buf_;		//写数据的缓冲区
     Connection *prev_conn_;				//前一个结点的指针
 	Connection *next_conn_;				//后一个结点的指针
+    
+    
+    BufferList read_buf_list_;
+    BufferList write_buf_list_;
+    char read_buf_[1024];
+//    char write_buf_[1024];
 };
 
 
